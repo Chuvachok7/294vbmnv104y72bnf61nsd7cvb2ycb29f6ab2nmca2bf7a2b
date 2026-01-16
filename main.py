6ea5f7537d368f2b2364551e1ed2afed2d43546c
Chuvachok7 <150792057+Chuvachok7@users.noreply.github.com>
2026-01-17 00:07:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if not API_TOKEN:
     print("Ошибка: BOT_TOKEN не найден в .env")
     sys.exit(1)
 
-ALLOWED_CHATS = [-1003569460645, -1002264373639, 5235155365]
+ALLOWED_CHATS = [-1003569460645, -1002264373639]
 CLEAN_TEXT = False
 
 def escape_html(text: str, /) -> str:
